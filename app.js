cfb7ea2f24a6b195363472c11ad4478a7cd10d76
Лисицын Даниил <daniil.lisitsyn@gazprombank.ru>
2024-10-16 18:01:35 +0300

    init mongoose, body-parser, morgan, config, nodemon

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const config = require("config");
+
+
+const app = express();
+const port = config.get('port');
+const mongoUri = config.get('mongoUri');
+
+mongoose.connect(mongoUri);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
